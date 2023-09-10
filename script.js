47ae78a029b2b677521d9de7401c75b8731a20f5
Iamneo <techadmin@iamneo.ai>
2023-09-10 11:35:45 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const boxs=document.querySelectorAll('.btn')
+const boxs=document.querySelectorAll('.btn');
+const statusTxt=document.querySelector('.result');
+const btnReset=document.querySelector('#reset');
+let x="<img src='images/x.png'>";
+let o="<img src='images/o.png'>";
+
+const win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
